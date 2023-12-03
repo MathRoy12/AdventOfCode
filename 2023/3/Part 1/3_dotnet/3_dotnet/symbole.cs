87aea7a7c492ade6645a2ea4dd692075b95c9e04
Mathieu Roy <2234476@cegepmontpetit.ca>
2023-12-03 15:36:26 -0500

    Finalisation Partie 1 du jours 3 de 2023

---
+++ 2023/3/Part 1/3_dotnet/3_dotnet/symbole.cs
@@ -0,0 +1,13 @@
+﻿namespace _3_dotnet;
+
+public struct symbole
+{
+    public int x;
+    public int y;
+    public char symbol;
+
+    public override string ToString()
+    {
+        return symbol + " (" + x + ", " + y + ")";
+    }
+}
---
+++ 2023/3/part 2/3_dotnet/3_dotnet/Program.cs
@@ -0,0 +1,275 @@
+﻿using System.Text.RegularExpressions;
+using _3_dotnet;
+
+StreamReader sr = new StreamReader("input.txt");
+
+string input = sr.ReadToEnd();
+
+string[] lines = input.Split("\n");
+List<symbole> listSymboles = new List<symbole>();
+
+int sum = 0;
+
+Regex rgx = new Regex("[^0-9.]");
+Regex rgx2 = new Regex("[0-9]");
+
+
+for (int i = 0; i < lines.Length; i++)
+{
+    for (int j = 0; j < lines[i].Length - 1; j++)
+    {
+        int lastNum = 0;
+        if (rgx.IsMatch(lines[i][j].ToString()))
+        {
+            Console.WriteLine(new symbole()
+            {
+                symbol = lines[i][j],
+                x = j,
+                y = i
+            });
+            if (rgx2.IsMatch(lines[i != 0 ? i - 1 : 0][j != 0 ? j - 1 : 0].ToString()) && i != 0 && j != 0)
+            {
+                int x = j - 1;
+                int y = i - 1;
+                string num = lines[i - 1][j - 1].ToString();
+
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x--;
+                    if (x < 0 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num = lines[y][x].ToString() + num;
+                }
+
+                x = j - 1;
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x++;
+                    if (x > lines[y].Length - 1 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num += lines[y][x].ToString();
+                }
+
+                if (int.Parse(num) != lastNum)
+                {
+                    sum += int.Parse(num);
+                    lastNum = int.Parse(num);
+                }
+            }
+
+            if (rgx2.IsMatch(lines[i - 1][j].ToString()))
+            {
+                int x = j;
+                int y = i - 1;
+                string num = lines[i - 1][j].ToString();
+
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x--;
+                    if (x < 0 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num = lines[y][x].ToString() + num;
+                }
+
+                x = j;
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x++;
+                    if (x > lines[y].Length - 1 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num += lines[y][x].ToString();
+                }
+
+                if (int.Parse(num) != lastNum)
+                {
+                    sum += int.Parse(num);
+                    lastNum = int.Parse(num);
+                }
+            }
+
+            if (rgx2.IsMatch(lines[i - 1][j + 1].ToString()))
+            {
+                int x = j + 1;
+                int y = i - 1;
+                string num = lines[i - 1][j + 1].ToString();
+
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x--;
+                    if (x < 0 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num = lines[y][x].ToString() + num;
+                }
+
+                x = j + 1;
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x++;
+                    if (x > lines[y].Length - 1 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num += lines[y][x].ToString();
+                }
+
+                if (int.Parse(num) != lastNum)
+                {
+                    sum += int.Parse(num);
+                    lastNum = int.Parse(num);
+                }
+            }
+
+            if (rgx2.IsMatch(lines[i][j - 1].ToString()))
+            {
+                int x = j - 1;
+                int y = i;
+                string num = lines[i][j - 1].ToString();
+
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x--;
+                    if (x < 0 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num = lines[y][x].ToString() + num;
+                }
+
+                x = j -1;
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x++;
+                    if (x > lines[y].Length - 1 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num += lines[y][x].ToString();
+                }
+
+                if (int.Parse(num) != lastNum)
+                {
+                    sum += int.Parse(num);
+                    lastNum = int.Parse(num);
+                }
+            }
+
+            if (rgx2.IsMatch(lines[i][j + 1].ToString()))
+            {
+                int x = j + 1;
+                int y = i;
+                string num = lines[i][j + 1].ToString();
+
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x--;
+                    if (x < 0 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num = lines[y][x].ToString() + num;
+                }
+
+                x = j + 1;
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x++;
+                    if (x > lines[y].Length - 1 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num += lines[y][x].ToString();
+                }
+
+                if (int.Parse(num) != lastNum)
+                {
+                    sum += int.Parse(num);
+                    lastNum = int.Parse(num);
+                }
+            }
+
+            if (rgx2.IsMatch(lines[i + 1][j - 1].ToString()))
+            {
+                int x = j - 1;
+                int y = i + 1;
+                string num = lines[i + 1][j - 1].ToString();
+
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x--;
+                    if (x < 0 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num = lines[y][x].ToString() + num;
+                }
+
+                x = j -1;
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x++;
+                    if (x > lines[y].Length - 1 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num += lines[y][x].ToString();
+                }
+
+                if (int.Parse(num) != lastNum)
+                {
+                    sum += int.Parse(num);
+                    lastNum = int.Parse(num);
+                }
+            }
+
+            if (rgx2.IsMatch(lines[i + 1][j].ToString()))
+            {
+                int x = j;
+                int y = i + 1;
+                string num = lines[i + 1][j].ToString();
+
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x--;
+                    if (x < 0 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num = lines[y][x].ToString() + num;
+                }
+
+                x = j;
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x++;
+                    if (x > lines[y].Length - 1 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num += lines[y][x].ToString();
+                }
+
+                if (int.Parse(num) != lastNum)
+                {
+                    sum += int.Parse(num);
+                    lastNum = int.Parse(num);
+                }
+            }
+
+            if (rgx2.IsMatch(lines[i + 1][j + 1].ToString()))
+            {
+                int x = j + 1;
+                int y = i + 1;
+                string num = lines[i + 1][j + 1].ToString();
+
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x--;
+                    if (x < 0 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num = lines[y][x].ToString() + num;
+                }
+
+                x = j + 1;
+                while (rgx2.IsMatch(lines[y][x].ToString()))
+                {
+                    x++;
+                    if (x > lines[y].Length - 1 || !rgx2.IsMatch(lines[y][x].ToString()))
+                        break;
+                    num += lines[y][x].ToString();
+                }
+
+                if (int.Parse(num) != lastNum)
+                {
+                    sum += int.Parse(num);
+                    lastNum = int.Parse(num);
+                }
+            }
+        }
+    }
+}
+
+Console.WriteLine(sum);
+
+sr.Close();
